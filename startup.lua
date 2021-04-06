92ad587c8f2af142348cefaf32ea07b3a166b5df
Jaja Steele <superjaja05@gmail.com>
2021-04-07 01:39:13 +0200

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -6,6 +6,9 @@ local r = component.nc_fusion_reactor
 local g = component.gpu
 
 term.setCursor(3,2)
+
+g.setBackground(0x0033DD)
+g.fill(1,1,160,50," ")
 g.setForeground(0x0055FF)
 g.setBackground(0x0033FF)
-g.fill(3,2,156,48,"+")
+g.fill(3,3,156,47," ")
