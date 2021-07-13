dc2feee00f68557db10881249786e0042f11c482
Jaja Steele <38012296+superjaja05@users.noreply.github.com>
2021-07-13 13:59:15 +0200

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -9,14 +9,14 @@ local g = component.gpu --this registers the graphic card , to use later! :3
 print("Hello user! Searching for updates..")
 os.sleep(1)
 shell.execute("cd /home") -- Now it does all the deleting old version , downloading new one , and then it launches uwu
-shell.execute("del fs-gui.lua")
+shell.execute("del fs-gui.lua") -- Deletes old one!
 print("--Deleted FS-Gui--")
-shell.execute("del startup.lua")
+shell.execute("del startup.lua") -- Delete the 2nd old one
 print("--Deleted Startup--")
-shell.execute("wget https://github.com/superjaja05/nc-fusion/blob/main/fs-gui.lua?raw=true")
+shell.execute("wget https://github.com/superjaja05/nc-fusion/blob/main/fs-gui.lua?raw=true") --download the new one
 print("--Downloaded latest FS-Gui--")
-shell.execute("wget https://github.com/superjaja05/nc-fusion/blob/main/startup.lua?raw=true")
+shell.execute("wget https://github.com/superjaja05/nc-fusion/blob/main/startup.lua?raw=true") --download the 2nd new one
 print("--Downloaded latest Startup--")
 print("<<<Now starting program..>>>")
 os.sleep(1)
-shell.execute("fs-gui.lua")
+shell.execute("fs-gui.lua") --starts the real program
